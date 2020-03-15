32730dd554d9d125b3c497b5ed6577b3015685d7
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2020-03-15 21:21:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1 || num == 0 || num == 1 
+    return false 
+  elsif (2..num - 1).each do |i| 
+    if num % i == 0 
+      return false 
+    end 
+  end 
+end 
+true 
+end
